8fc0dc7e51e35af4e93171ac182fb50cf20c5c24
Bloodzila <r0414822@student.vives.be>
2014-11-14 19:22:36 +0100

    Test

--- app/src/main/java/be/vives/citroentjes/sportrijk/interfaces/ODKJson.java
+++ app/src/main/java/be/vives/citroentjes/sportrijk/interfaces/ODKJson.java
@@ -1,7 +1,13 @@
 package be.vives.citroentjes.sportrijk.interfaces;
 
+import java.util.ArrayList;
+
+import be.vives.citroentjes.sportrijk.model.Sport;
+
 /**
  * Created by Ik on 14/11/2014.
  */
-public interface ODKJson {
+public interface ODKJson
+{
+    public void updateArray(ArrayList<Sport> sportLijst);
 }
