b041860fd9c326d97831121d3159be64a8b8e791
Mytherious <b.alleweireldt@gmail.com>
2014-11-14 22:20:34 +0100

    Start voor schermwissel

---
+++ app/src/main/java/be/vives/citroentjes/sportrijk/LoginFragment.java
@@ -0,0 +1,47 @@
+package be.vives.citroentjes.sportrijk;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import be.vives.citroentjes.sportrijk.interfaces.OnFragmentInteractionListener;
+
+
+public class LoginFragment extends Fragment {
+    private OnFragmentInteractionListener mListener;
+    private View view;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view=inflater.inflate(R.layout.fragment_login, container, false);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(false);
+        return view;
+    }
+
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+}
---
+++ app/src/main/java/be/vives/citroentjes/sportrijk/interfaces/OnFragmentInteractionListener.java
@@ -0,0 +1,8 @@
+package be.vives.citroentjes.sportrijk.interfaces;
+
+/**
+ * Created by Mytherious on 14/11/2014.
+ */
+public interface OnFragmentInteractionListener {
+    public void showNextFragment(int button);
+}
