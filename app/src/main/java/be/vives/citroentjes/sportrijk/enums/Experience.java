f2fbb5e08af06686a842cafb92760835e6c35d2b
Cedric-Cornelis <cedric.cornelis@student.vives.be>
2014-11-14 19:24:32 +0100

    Domeinklas Person toegevoegd

---
+++ app/src/main/java/be/vives/citroentjes/sportrijk/enums/Experience.java
@@ -0,0 +1,10 @@
+package be.vives.citroentjes.sportrijk.enums;
+
+/**
+ * Created by Cedric Cornelis on 14/11/2014.
+ */
+public enum Experience {
+
+    AMATEUR, ERVAREN, PROFESSIONAL
+
+}
---
+++ app/src/main/java/be/vives/citroentjes/sportrijk/model/Person.java
@@ -0,0 +1,20 @@
+package be.vives.citroentjes.sportrijk.model;
+
+import java.util.ArrayList;
+
+import be.vives.citroentjes.sportrijk.enums.Experience;
+
+/**
+ * Created by Cedric Cornelis on 14/11/2014.
+ */
+public class Person {
+
+    private String name;
+    private String surName;
+    private int age;
+    private String userName;
+    private String passWord;
+    private Experience experience;
+    private ArrayList<Person> friends;
+
+}
