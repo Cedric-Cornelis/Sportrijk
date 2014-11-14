a55fd73c810ebb05fa71b4b0dde7034edfe51404
Bloodzila <r0414822@student.vives.be>
2014-11-14 20:16:43 +0100

    Test

--- app/src/main/java/be/vives/citroentjes/sportrijk/model/Sport.java
+++ app/src/main/java/be/vives/citroentjes/sportrijk/model/Sport.java
@@ -3,5 +3,84 @@ package be.vives.citroentjes.sportrijk.model;
 /**
  * Created by Ik on 14/11/2014.
  */
-public class Sport {
+public class Sport
+{
+
+    private String adres;
+    private String gemeente;
+    private String soort;
+    private String sport;
+    private double longitude;
+    private double latitude;
+
+    public Sport()
+    {
+
+    }
+
+    public Sport(String adres, String gemeente, String soort, String sport, double longitude, double latitude)
+    {
+        this.adres = adres;
+        
+    }
+
+    public String getAdres()
+    {
+        return adres;
+    }
+
+    public void setAdres(String adres)
+    {
+        this.adres = adres;
+    }
+
+    public String getGemeente()
+{
+    return gemeente;
+}
+
+    public void setGemeente(String gemeente)
+    {
+        this.gemeente = gemeente;
+    }
+
+    public String getSoort()
+    {
+        return soort;
+    }
+
+    public void setSoort(String soort)
+    {
+        this.soort = soort;
+    }
+
+    public String getSport()
+    {
+        return sport;
+    }
+
+    public void setSport(String sport)
+    {
+        this.sport = sport;
+    }
+
+    public double getLatitude()
+    {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude)
+    {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude()
+    {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude)
+    {
+        this.longitude = longitude;
+    }
 }
