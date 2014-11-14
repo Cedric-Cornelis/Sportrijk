087227b33f1c07142546e21c38766bf59c948ef7
Cedric-Cornelis <cedric.cornelis@student.vives.be>
2014-11-14 19:28:32 +0100

    person methodes toegevoegd

--- app/src/main/java/be/vives/citroentjes/sportrijk/model/Person.java
+++ app/src/main/java/be/vives/citroentjes/sportrijk/model/Person.java
@@ -17,4 +17,64 @@ public class Person {
     private Experience experience;
     private ArrayList<Person> friends;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public Experience getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Experience experience) {
+        this.experience = experience;
+    }
+
+    public ArrayList<Person> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(ArrayList<Person> friends) {
+        this.friends = friends;
+    }
+
+    public void addFriends(Person person)
+    {
+        friends.add(person);
+    }
 }
