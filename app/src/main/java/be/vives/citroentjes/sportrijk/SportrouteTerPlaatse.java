691a06c5410690bd5c1a673303909c906a2c91cf
Jorrex <verstroate_jorre@hotmail.com>
2014-11-15 06:22:02 +0100

    ken oengr

--- app/src/main/java/be/vives/citroentjes/sportrijk/SportrouteTerPlaatse.java
+++ app/src/main/java/be/vives/citroentjes/sportrijk/SportrouteTerPlaatse.java
@@ -1,7 +1,58 @@
 package be.vives.citroentjes.sportrijk;
 
-/**
- * Created by Jorre on 15/11/2014.
- */
-public class SportrouteTerPlaatse {
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import be.vives.citroentjes.sportrijk.interfaces.OnFragmentInteractionListener;
+
+
+public class SportrouteTerPlaatse extends Fragment {
+    private OnFragmentInteractionListener mListener;
+    private View view;
+
+    public SportrouteTerPlaatse() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view=inflater.inflate(R.layout.fragment_login, container, false);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(false);
+        Button btnNextActivity = (Button) view.findViewById(R.id.btnNextActivity);
+
+        btnNextActivity.setOnClickListener(btnNextActivityClicked);
+        return view;
+    }
+
+    private View.OnClickListener btnNextActivityClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //Navigatie naar de volgende pagina - kweetnie welkeen dat is, you might know ;)
+        }
+    };
+
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
 }
